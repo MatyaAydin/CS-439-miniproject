e92c6396d5c667aaf31f7133ef6a499f26dfcbaf
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2025-06-13 00:25:36 +0200

    add cifar extensions

--- src/optimizers/mrcg.py
+++ src/optimizers/mrcg.py
@@ -5,14 +5,13 @@ import jax.random as jr
 from collections.abc import Callable
 
 def scaling_selection(g, loss_at_params, params,
-                      sigma, key, constant_learning_rate=True, hv_fun=None):
+                      sigma, key, constant_learning_rate=True, use_H_hat=False, H_hat=None, hv_fun=None):
     """Choose a scaled descent direction p = -s·g according to curvature."""
-    # Hessian–vector product  H g
-    # Hg = jax.jvp(lambda p: jax.grad(loss_at_params)(p, key),
-    #              (params,), (g,))[1]
-    Hg = hv_fun(params, g) if hv_fun is not None else \
-         jax.jvp(lambda p: jax.grad(loss_at_params)(p, key),
-                 (params,), (g,))[1]
+
+    if not use_H_hat:
+        Hg = hv_fun(params, g)
+    else:
+        Hg = hv_fun(g, H_hat)
 
     dot_product = jnp.dot(g, Hg)
     norm_g      = jnp.linalg.norm(g)
@@ -48,30 +47,7 @@ def scaling_selection(g, loss_at_params, params,
         snc  = jr.uniform(key, shape=(), minval=s_lpc_min, maxval=s_lpc_max)
         return -snc * g, "NC"
 
-# def scaling_selection(g, loss_at_params, params, sigma, key, constant_learning_rate=True):
-#     Hg = jax.jvp(lambda p: jax.grad(loss_at_params)(p, key), (params,), (g,))[1]
-#     dot_product = jnp.dot(g, Hg)
-#     norm_g =jnp.linalg.norm(g)
 
-#     if constant_learning_rate:
-#         s_lpc_min = 1 / sigma #set to 1/sigma for constant learning rate
-#         s_lpc_max = 1 / sigma #set to 1/sigma for constant learning rate
-#     else:
-#         s_lpc_min = 1 / sigma *jr.random(key)
-
-#     s_CG =jnp.linalg.norm(g)**2 / dot_product
-#     s_MR = dot_product /jnp.linalg.norm(Hg)**2
-#     s_GM =jnp.sqrt(s_CG * s_MR)
-
-#     if dot_product > sigma * norm_g**2:
-#         spc =jr.choice(key, a=jnp.array([s_CG, s_MR, s_GM]))
-#         return -spc*g, "SPC"
-#     elif 0 < dot_product and dot_product < sigma * norm_g**2:
-#         slpc =jr.uniform(key, s_lpc_min, 1 / sigma)
-#         return -slpc * g, "LPC"
-#     else:
-#         snc =jr.uniform(key, s_lpc_min, s_lpc_max)
-#         return -snc * g, "NC"
 
 def backtracking_LS(loss_at_params, key, theta, rho, x, g, p):
 
@@ -94,43 +70,7 @@ def forward_backward_LS(loss_at_params, key, theta, rho, x, g, p):
 
     return alpha * theta
 
-# algorithm 2: scaled gradient descent with line search
 
-
-# def scaled_GD(loss_at_params, key, x0, sigma, rho, theta_bt, theta_fb, MAX_ITER, eps):
-#     """
-#     sigma <<< 1
-#     0 < theta < 1
-#     0 < rho < 1/2
-#     """
-
-#     x_k = x0
-#     flag_distribution = {"SPC": 0, "LPC": 0, "NC": 0}
-
-
-#     for _ in range(MAX_ITER):
-
-#         g_k = 2 * x_k
-
-#         if jnp.linalg.norm(g_k) < eps:
-#             break
-        
-
-#         p_k, FLAG = scaling_selection(g_k, loss_at_params, x_k, jnp.eye(len(x_k)), sigma)
-#         flag_distribution[FLAG] += 1
-
-#         if FLAG == "SPC" or FLAG == "LPC":
-#             alpha_k = backtracking_LS(loss_at_params, key, theta_bt, rho, x_k, g_k, p_k)
-
-#         else:
-#             alpha_k = forward_backward_LS(loss_at_params, key, theta_fb, rho, x_k, g_k, p_k)
-
-#         x_k += alpha_k * p_k
-
-#     return x_k, flag_distribution
-
-
-#algorithm 3 backward tracking line search
 
 def backtracking_LS(loss_at_params, key, theta, rho, x, g, p):
 
@@ -141,8 +81,7 @@ def backtracking_LS(loss_at_params, key, theta, rho, x, g, p):
 
     return alpha
 
-def Hv_product(f, params, vec, key):
-    return 
+
 
 
 @dataclass
